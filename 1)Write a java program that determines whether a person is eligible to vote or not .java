2e552aea434969aff617feb4025574dd71b24f47
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2025-08-07 18:34:13 +0530

    Create 1)Write a java program that determines whether a person is eligible to vote or not .java

---
+++ 1)Write a java program that determines whether a person is eligible to vote or not .java
@@ -0,0 +1,15 @@
+class Vote
+{
+    public static void main(String[] args)
+    {
+        int age = 15;
+        if(age >= 18)
+        {
+            System.out.println("Eligible to vote");
+        }
+        else
+        {
+            System.out.println("Not Eligible to vote");
+        }
+    }
+}
