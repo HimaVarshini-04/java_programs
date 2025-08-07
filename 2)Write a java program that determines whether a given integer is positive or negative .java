d0d0fa9b2563e238ae69de43b2d515e98b68b38e
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2025-08-07 18:36:27 +0530

    Create 2)Write a java program that determines whether a given integer is positive or negative .java

---
+++ 2)Write a java program that determines whether a given integer is positive or negative .java
@@ -0,0 +1,15 @@
+class Number
+{
+    public static void main(String[] args)
+    {
+        int a = 15;
+        if(a >= 0)
+        {
+            System.out.println( a+ "is Positive");
+        }
+        else
+        {
+            System.out.println( a + "is Negative");
+        }
+    }
+}
