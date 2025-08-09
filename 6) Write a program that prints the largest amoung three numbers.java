1bd88ccb0d182706be882996c553663895f39780
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2025-08-09 12:13:56 +0530

    Create 6) Write a program that prints the largest among three numbers.java

---
+++ 6) Write a program that prints the largest amoung three numbers.java
@@ -0,0 +1,19 @@
+class Largest
+{
+  public static void main(String[] args)
+  {
+    int a = 10 ,b = 20 , c = 30;
+    if(a>b && a>c)
+    {
+     System.ot.println(a + " is largest ");
+    }
+    else if(b>a && b>c )
+    {
+     System.ot.println(b + " is largest ");
+    }
+    else
+    {
+     System.ot.println(c + " is largest ");
+    }
+  }
+}
