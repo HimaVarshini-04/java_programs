4b156ad80ee5cde1612855270fe9815f6dfbe49b
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2025-08-09 12:16:26 +0530

    Create 7) Write a program that determines whether the given alphabet is vowel or consonents.java

---
+++ 7) Write a program that determines whether the given alphabet is vowel or consonents.java
@@ -0,0 +1,15 @@
+class Vowels 
+{
+	public static void main(String[] args) 
+	{
+		char v='E';
+		if(v =='a'|| v =='e'|| v =='i' || v =='o'||v =='u'||v =='A'|| v =='E'||v =='I'||v =='O'|| v =='U')
+		{
+				System.out.println(v+" is vowel");
+	  }
+		else
+		{
+			System.out.println(v+" is consonent");
+		}
+	}	
+}
