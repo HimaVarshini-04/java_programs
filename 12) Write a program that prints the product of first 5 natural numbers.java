74bc9244d7f71ab3262c44b27a0aa3b5079ad310
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2025-08-09 12:31:27 +0530

    Create 12) Write a program that prints the product of first 5 natural numbers.java

---
+++ 12) Write a program that prints the product of first 5 natural numbers.java
@@ -0,0 +1,12 @@
+class ProdNaturalNumbers 
+{
+	public static void main(String[] args) 
+	{
+		int product=1,n=5;
+		for(int i=1;i<=n;i++)
+		{
+		  	prod*=i;
+		}
+		System.out.println(product);
+	}
+}
