8f7ab22177028cabd0244c5b929ac2f5b723c8e8
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2025-08-09 12:07:15 +0530

    Create 4) Write a program that prints largest among two numbers where two numbers are not equal.java

---
+++ 4) Write a program that prints largest among two numbers where two numbers are not equal.java
@@ -0,0 +1,15 @@
+class Largest
+{
+  public static void main(String[] args)
+  {
+    int a = 10 ,b = 20;
+    if(a>b)
+    {
+     System.ot.println(a + " is largest ");
+    }
+    else
+    {
+     System.ot.println(b + " is largest ");
+    }
+ }
+}
